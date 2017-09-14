b3fdbc50e5c666e54d22b3e6162137b404eed528
91086732 <31664258+91086732@users.noreply.github.com>
2017-09-14 14:30:16 -0700

    Houses Variations

--- src/org/teachingkidsprogramming/section02methods/Houses.java
+++ src/org/teachingkidsprogramming/section02methods/Houses.java
@@ -9,26 +9,37 @@ public class Houses
   {
     Tortoise.show();
     Tortoise.setSpeed(10);
-    Tortoise.setX(10);
+    Tortoise.setX(200);
     int height = 40;
-    for (int i = 0; i < 3; i++)
-    {
-      drawHouse(height);
-      drawHouse(120);
-      drawHouse(90);
-      drawHouse(20);
-    }
+    drawHouse(height);
+    // drawHouse(120);
+    //drawHouse(90);
+    //drawHouse(20);
   }
   private static void drawHouse(int height)
   {
-    Tortoise.setPenColor(PenColors.Grays.LightGray);
+    Tortoise.setPenColor(PenColors.Reds.Red);
     Tortoise.move(height);
-    Tortoise.turn(90);
+    Tortoise.turn(45);
     Tortoise.move(30);
-    Tortoise.turn(90);
+    Tortoise.turn(45);
     Tortoise.move(height);
+    Tortoise.turn(45);
+    Tortoise.move(30);
+    Tortoise.turn(45);
+    Tortoise.move(30);
     Tortoise.turn(-90);
     Tortoise.move(20);
+    Tortoise.turn(-90);
+    Tortoise.move(120);
+    Tortoise.turn(45);
+    Tortoise.move(30);
+    Tortoise.turn(45);
+    Tortoise.move(height);
+    Tortoise.turn(45);
+    Tortoise.move(30);
+    Tortoise.turn(45);
+    Tortoise.move(120);
     Tortoise.turn(-90);
   }
 }
