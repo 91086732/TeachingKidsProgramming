cf6ae52e61e5a1ec31c86621459da46db5d9ef2b
91086732 <31664258+91086732@users.noreply.github.com>
2017-09-15 13:34:39 -0700

    Houses

--- src/org/teachingkidsprogramming/section02methods/Variations/HousesSlanted.java
+++ src/org/teachingkidsprogramming/section02methods/Variations/HousesSlanted.java
@@ -1,16 +1,16 @@
-package org.teachingkidsprogramming.section02methods;
+package org.teachingkidsprogramming.section02methods.Variations;
 
 import org.teachingextensions.logo.Tortoise;
 import org.teachingextensions.logo.utils.ColorUtils.PenColors;
 
-public class Houses
+public class HousesSlanted
 {
   public static void main(String[] args)
   {
     Tortoise.show();
     Tortoise.setSpeed(10);
     Tortoise.setX(200);
-    int height = 40;
+    int height = 36;
     drawHouse(height);
     drawHouse(120);
     drawHouse(90);
@@ -20,10 +20,10 @@ public class Houses
   {
     Tortoise.setPenColor(PenColors.Reds.Red);
     Tortoise.move(height);
-    Tortoise.turn(90);
+    Tortoise.turn(100);
     Tortoise.move(30);
-    Tortoise.turn(90);
-    Tortoise.move(height);
+    Tortoise.turn(80);
+    Tortoise.move(height - 4);
     Tortoise.turn(-90);
     Tortoise.move(20);
     Tortoise.turn(-90);
