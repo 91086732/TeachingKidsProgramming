b2b7321762dc7440ba4f2b5b90bc0f632f0d969e
91086732 <31664258+91086732@users.noreply.github.com>
2017-09-19 09:40:35 -0700

    Section 2 Complete

--- src/org/teachingkidsprogramming/section02methods/FourSquare.java
+++ src/org/teachingkidsprogramming/section02methods/FourSquare.java
@@ -1,30 +1,27 @@
 package org.teachingkidsprogramming.section02methods;
 
 import org.teachingextensions.logo.Tortoise;
-import org.teachingextensions.logo.utils.ColorUtils.ColorWheel;
-import org.teachingextensions.logo.utils.ColorUtils.PenColors.Blues;
-import org.teachingextensions.logo.utils.ColorUtils.PenColors.Greens;
-import org.teachingextensions.logo.utils.ColorUtils.PenColors.Reds;
+import org.teachingextensions.logo.utils.ColorUtils.PenColors;
 
 public class FourSquare
 {
   public static void main(String[] args)
   {
     Tortoise.show();
-    //  Make the tortoise move as fast as possible --#7
-    //  Do the following 4 times --#8.1
-    //      drawSquare (recipe below) --#6.1
-    //      ------------- Recipe for drawSquare --#6.2
+    Tortoise.setSpeed(10);
     for (int i = 0; i < 4; i++)
     {
-      ColorWheel.addColor(Reds.Crimson);
-      ColorWheel.addColor(Blues.Blue);
-      ColorWheel.addColor(Greens.Chartreuse);
+      drawSquare();
+      Tortoise.turn(90);
+    }
+  }
+  private static void drawSquare()
+  {
+    for (int i = 0; i < 4; i++)
+    {
+      Tortoise.setPenColor(PenColors.getRandomColor());
       Tortoise.move(50);
       Tortoise.turn(90);
-      //      ------------- End of drawSquare recipe --#6.3
-      //      Turn the tortoise 90 degrees to the right --#9
-      //  End Repeat --#8.2
     }
   }
 }
