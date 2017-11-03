310bb78a1853465229248549c07692736f624623
91086732 <31664258+91086732@users.noreply.github.com>
2017-11-03 13:33:16 -0700

    BackPhotoDone

--- src/org/teachingkidsprogramming/section04mastery/BackgroundPhoto.java
+++ src/org/teachingkidsprogramming/section04mastery/BackgroundPhoto.java
@@ -1,23 +1,24 @@
 package org.teachingkidsprogramming.section04mastery;
 
 import org.teachingextensions.logo.Tortoise;
-import org.teachingextensions.logo.utils.ColorUtils.PenColors.Yellows;
+import org.teachingextensions.logo.utils.ColorUtils.PenColors;
 
 public class BackgroundPhoto
 {
   @SuppressWarnings("unused")
   public static void main(String[] args)
   {
-    Tortoise.getBackgroundWindow()
-        .setBackgroundImage("https://usatftw.files.wordpress.com/2017/05/spongebob.jpg?w=1000&h=600&crop=1");
+    String currPicture = "https://usatftw.files.wordpress.com/2017/05/spongebob.jpg?w=1000&h=600&crop=1";
+    Tortoise.getBackgroundWindow().setBackgroundImage(currPicture);
     Tortoise.show();
     Tortoise.setSpeed(10);
     int side = 2;
     for (int i = 0; i < 75; i++)
     {
-      Tortoise.setPenColor(Yellows.Yellow);
-      int length = side + 1;
-      Tortoise.move(length);
+      java.awt.Color currColor = PenColors.Yellows.Yellow;
+      Tortoise.setPenColor(currColor);
+      side++;
+      Tortoise.move(side);
       Tortoise.setX(555);
       Tortoise.setY(65);
       Tortoise.turn(360 / 3 + 1);
