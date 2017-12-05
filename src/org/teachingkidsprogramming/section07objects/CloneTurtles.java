cdf1d813cc9f47b3cd9211244e6a7456bb536a67
91086732 <31664258+91086732@users.noreply.github.com>
2017-12-05 09:20:07 -0800

    ST

--- src/org/teachingkidsprogramming/section07objects/CloneTurtles.java
+++ src/org/teachingkidsprogramming/section07objects/CloneTurtles.java
@@ -1,10 +1,14 @@
 package org.teachingkidsprogramming.section07objects;
 
+import java.util.ArrayList;
+
+import org.teachingextensions.WindowUtils.MultiTurtleWindow;
+import org.teachingextensions.logo.Turtle;
+
 public class CloneTurtles
 {
-  // Uncomment the two lines of code below to create a container and a window for your turtles --#1
-  // public ArrayList<Turtle> turtles = new ArrayList<Turtle>();
-  // public MultiTurtleWindow mtw     = new MultiTurtleWindow();
+  public ArrayList<Turtle> turtles = new ArrayList<Turtle>();
+  public MultiTurtleWindow mtw     = new MultiTurtleWindow();
   //
   public CloneTurtles()
   {
@@ -14,11 +18,12 @@ public class CloneTurtles
   private void showSomeTurtles()
   {
     // Use a FOR loop to add your three turtles --#4.1
-    //      Create a turtle instance  --#2.1
-    //      Add your turtles to your turtle container --#2.2
+    Turtle turtle = new Turtle();
+    mtw.addAndShowTurtle(turtle);
     // End Repeat --#4.2
     //
     // Create a variable to count the number of turtles in the container --#3.1
+    int counter = 0;
     // Uncomment to Show the number of turtles in the container on the window --#3.2
     // new Text("There are " + numberOfTurtles + " turtles in the turtle container").setTopLeft(50, 50).setPenColor(PenColors.Yellows.Gold).addTo(mtw);
     //
